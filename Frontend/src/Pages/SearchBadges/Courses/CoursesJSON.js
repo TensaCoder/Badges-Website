668f325894c784f285e68ef9413c3c30199bd6f3
TensaCoder <herschel.menezes00@gmail.com>
2024-05-08 12:44:30 +0530

    CourseJSON created to store the objects path

---
+++ Frontend/src/Pages/SearchBadges/Courses/CoursesJSON.js
@@ -0,0 +1,13 @@
+export const courseImages = {
+    //   Bronze : "../../../../../public/Images/Internship/Bronze.png",
+    "Cyber Law" : "./Images/CyberLaw.png",
+    "Dark Web": "./Images/DarkWeb.png",
+      Forensic: "./Images/Forensic.png",
+      OSINT: "./Images/OSINT.png",
+      "Ransomware Research": "./Images/RansomwareResearch.png",
+      "Red Teaming": "./Images/RedTeaming.png",
+      "Blue Teaming": "./Images/BlueTeaming.png",
+      "Web Development": "./Images/WebDevelopment.png",
+      // Add more badge names as needed
+    };
+    
